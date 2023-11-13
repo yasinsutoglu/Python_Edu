e985ce99148d305e4b4ab347926a776d525b7c4e
Yasin Sutoglu <ysutoglu@gmail.com>
2023-11-13 16:41:19 +0300

    ilk ders

---
+++ Python_Intro/hello.py
@@ -0,0 +1,13 @@
+print("hello")
+print(2+2) #comment
+print("""nabiyon lan docString bu""")
+
+name = str(input())
+age = int(input())
+print('Hi,{name}, you are {age}.') #template literal
+
+""" MULTILINE COMMENT -
+"Hi {}, you are {}".format(name, age)
+ "Hi {name}, you are {age}".format(name=name, age=age)
+ "Hi %s, you are %d" % (name, age)
+ """
