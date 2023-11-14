e1da1444d1c03f73ec6c14d7d85060e7a07f6e7b
Yasin Sutoglu <ysutoglu@gmail.com>
2023-11-14 21:46:07 +0300

    basics

--- Python_Intro/hello.py
+++ Python_Intro/hello.py
@@ -11,3 +11,24 @@ print('Hi,{name}, you are {age}.') #template literal
  "Hi {name}, you are {age}".format(name=name, age=age)
  "Hi %s, you are %d" % (name, age)
  """
+ 
+""" Farklı type'larda oto string concatination pythonda yok typecasting yada template literal kullanırız"""
+
+""" dir=> tüm methodları göstermek için kullanılır."""
+
+email= "yasin@gmail.com"
+print(email[::-1])
+print(email[3:])
+print(email[::2])
+
+number = "1234"
+print(number.lower())
+print(number.islower())
+print(number.isnumeric())
+
+first_name = "yasin"
+last_name = "sutoglu"
+age = 19
+# fstring diye geçiyor js template literal ile aynı
+full_name = f"{first_name} {last_name} {age} yasindadir. Seneye {age + 1} yasinda olacaktir."
+print(full_name)
