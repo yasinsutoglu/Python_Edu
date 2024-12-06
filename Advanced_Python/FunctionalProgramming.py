246d0f32d465ee0347da370705d12ccfafcca1b6
Yasin Sutoglu <ysutoglu@gmail.com>
2024-12-06 20:42:32 +0300

    func programming commenced

---
+++ Advanced_Python/FunctionalProgramming.py
@@ -0,0 +1,10 @@
+# WHAT IS FUNCTIONAL PROGRAMMING?
+# It is about separation of concerns as OOP. But, Separate data and functions.
+# Instead of combining methods and attributes, we separate them apart.
+# Functions use lists, dictionaries instead of Class(Objects)
+# Packaging our codes to make it well-organized.
+# Objectives:
+# Clear + Understandable Codes, Easy to Extend, Easy to Maintain, Memory Efficient, DRY
+# One Pillar ==> Pure Functions
+# ---------------------------------------
+# PURE FUNCTIONS
