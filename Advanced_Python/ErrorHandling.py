1b99e40f9b75d024458b9c750bff6899f2672c69
Yasin Sutoglu <ysutoglu@gmail.com>
2024-12-09 15:31:22 +0300

    error handling end

--- Advanced_Python/ErrorHandling.py
+++ Advanced_Python/ErrorHandling.py
@@ -1,25 +1,66 @@
 # ERR_Handling-1
-while True:
-	try:
-		age = int(input("Enter your age: "))
-		age_in_dogs_year = 10 / age
+# Error types => ValueError, SyntaxError, NameError, ZeroDivisionError, TypeError,
+# IndexError, KeyError etc...
+# https://docs.python.org/3/library/exceptions.html (BUILT-IN Exceptions)
 
-	except ZeroDivisionError:
-		print("enter age greater than 0")
-		continue
+# EX-1;
+# while True:
+# 	try:
+# 		age = int(input("Enter your age: "))
+# 		age_in_dogs_year = 10 / age
+#
+#   except: # --> only except contains any ErrorType!!
+#       #...codes
+#
+# 	except ZeroDivisionError:
+# 		print("enter age greater than 0")
+# 		continue
+#
+# 	except ValueError:
+# 		print("Please enter a no.")
+# 		break
+#
+# 	except TypeError:
+# 		print("!!!!")
+#       break
+#
+# 	else: # else--> If there is no error in try-block, DO this code block!!!
+# 		print(f"thank you, and your age is {age}")
+# 		break
+#
+# 	finally: # Always do this at the end of the program, in any case!!
+# 		print("I will always get printed no matter what :)")
+#
+# 	print("can you hear me??????") -> this hasn't been printed!
+'''-----------------------------'''
+# EX-2;
+# def division_fn(num1, num2):
+#     try:
+#         return num1/num2
+#     except (ZeroDivisionError, TypeError) as err:
+#         print(f'error: {err}')
+#
+# division_fn(1,'0') # error: unsupported operand type(s) for /: 'int' and 'str'
+# division_fn(1,0) # error: division by zero
+# print(division_fn(1,4)) # 0.25
 
-	except ValueError:
-		print("Please enter a no.")
-		break
+# -----------RAISE AN ERROR----------
+# we can stop the program by raising our own errors. It's useful while creating your own lib.
 
-	except ValueError:
-		print("!!!!")
+# Ex-1
+# print("Hello!!!!")
 
-	else:
-		print(f"thank you, and your age is {age}")
-		break
+# raise TypeError("yo")
+# raise Exception("Any message!!")
 
-	finally:
-		print("I will always get printed no matter what :)")
+# print("bye")
 
-	print("can you hear me??????")
+# Ex-2
+# try:
+#     age = int(input("age: "))
+#     age = 10/age
+#     raise ValueError("Ending the program")
+#     # raise Exception("quit")
+#
+# except ValueError:
+#     print("Please enter a no.")
