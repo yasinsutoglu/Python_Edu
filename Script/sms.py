83142fce268cd95573517454dd4d7dd2f7982d55
Yasin Sutoglu <ysutoglu@gmail.com>
2024-12-11 21:57:36 +0300

    sms issue end

--- Script/sms.py
+++ Script/sms.py
@@ -1,13 +1,14 @@
 from twilio.rest import Client
+# USE --> Twilio "Programmable SMS" section
 
 account_sid = 'AC35a43368114f115baac776029167317c'
 auth_token = '8cfe654e7fa576d7961f8698f0b015fd'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    from_='+19284823038',
-    body='Hello Saurabh, this is your bot JARVIS! Good Morning!',
-    to='+919652723113'
+    from_='<your_twilio_phone_number>',
+    body='Hello JESS, this is your bot CITIR! Good Morning!',
+    to='<receiver_phone_number>'
     )
 
 print(message.sid)
@@ -22,9 +23,9 @@ client = Client(account_sid, auth_token)
 
 message = client.messages.create(
          # media_url=['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
-         from_='whatsapp:+14155238886',
-         body="Hello Saurabh. This is your bot: JARVIS, reporting on duty!",
-         to='whatsapp:+919652723113'
+         from_='whatsapp:<your_twilio_phone_number>',
+         body="Hello Jess. This is your bot: CITIR, reporting on duty!",
+         to='whatsapp:<receiver_phone_number>'
          )
 
 print(message.sid)
